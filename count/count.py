fb340bb81cc7b1294062940e54211052a4afa146
abhishekrajdisys <136111113+abhishekrajdisys@users.noreply.github.com>
2023-07-28 14:57:35 +0530

    testing credential accesss

--- count/count.py
+++ count/count.py
@@ -1,2 +1,9 @@
-for i in range(10):
-  print(i)
+import os
+
+def get_credentials():
+  if 'CI' in os.environ:
+    db_username = os.environ.get('USERNAME')
+  else:
+    db_username = 'your_local_username'
+  print(db_username)
+get_credentials()
